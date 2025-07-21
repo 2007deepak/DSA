a36810afbd365b770c04b78461aaed312a500629
Deepak Kumar <dk4503835@gmail.com>
2025-07-22 00:48:55 +0530

    added infine loop

--- basic_of_c++/loop.cpp
+++ basic_of_c++/loop.cpp
@@ -3,19 +3,30 @@ using namespace std;
 
 int main() {
 
-     for(int i=0; i<10; i++){
+    // for(int i=0; i<10; i++){
     //     cout<< i << endl;
 
     //cout<< "Deepak Kumar" << endl;
-        if(i % 2 == 0){
-            cout << i << ": is Even" << endl;
-        }
-        else{
-            cout << i << ": is Odd" << endl;
-        }
-     }
+    //     if(i % 2 == 0){
+    //         cout << i << ": is Even" << endl;
+    //     }
+    //     else{
+    //         cout << i << ": is Odd" << endl;
+    //     }
+    //  }
 
+    int i = 1;
+    for(; ;) {
+        cout << "value of i is:" << i << endl;
+        if(i<5){
+            cout << "Deepak " << endl;
+            i++;
+        }else{
 
+        }
+    }
+
+    cout << "Hi" << endl;
 
     return 0;
     }

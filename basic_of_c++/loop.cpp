25daadaf6382b6c16011c6c2d1377714ea48ac3f
Deepak Kumar <dk4503835@gmail.com>
2025-07-22 00:16:09 +0530

    using loop and if else

---
+++ basic_of_c++/loop.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+     for(int i=0; i<10; i++){
+    //     cout<< i << endl;
+
+    //cout<< "Deepak Kumar" << endl;
+        if(i % 2 == 0){
+            cout << i << ": is Even" << endl;
+        }
+        else{
+            cout << i << ": is Odd" << endl;
+        }
+     }
+
+
+
+    return 0;
+    }

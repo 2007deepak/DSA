75a1bd4ad0ad65989fe097b048d20eb505943924
Deepak Kumar <dk4503835@gmail.com>
2025-07-21 23:32:48 +0530

    conditional if else,elseif use

---
+++ basic_of_c++/conditionals.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int num = 7;
+
+    if(num%2 == 0){
+        cout << "Number is Even"<<endl;
+    }
+    else {
+        cout << "Number is Odd"<<endl;
+    }
+
+
+
+    // int num = 10;
+    // if(num >0){
+    //     cout << "Number is Positive"<< endl;
+    // }
+    // else if(num < 0){
+    //     cout << "Number is Negative"<< endl;
+    // }
+    // else {
+    //     cout << "Number is zero" << endl;
+    // }
+    
+    return 0;
+}

c77359df312aaf1c58d16c920f5d1243391e3917
Deepak Kumar <dk4503835@gmail.com>
2025-07-22 00:31:32 +0530

    pattern printing question using for loop

---
+++ basic_of_c++/pattern.cpp
@@ -0,0 +1,18 @@
+// patern printing in c++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+        // patern 1
+        for(int i=0; i<3; i++)
+        {
+            for(int j=1; j<6; j++)
+            {
+                cout<< "*";
+            }
+            cout << endl;
+        }
+        
+}

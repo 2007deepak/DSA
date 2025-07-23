186ed01d6accd6b7c1394c0c41817af3920ccbee
Deepak Kumar <dk4503835@gmail.com>
2025-07-23 11:22:22 +0530

    Pattern Printing Hollow Inverted Half Pyramid

--- basic_of_c++/pattern.cpp
+++ basic_of_c++/pattern.cpp
@@ -102,20 +102,17 @@ int main() {
             // *    *
             // *  *
             // *
-            for(int i =0; i<5; i++){
+            for(int i = 0; i<10; i++){
 
-            for( int j=0 ; j<5-1; j++){
-                if(i==0 || j==0 || j== 5-i-1 ){
-                    cout << " * ";
+            for( int j=0 ; j<10; j++){
+                if(i==0 || j==0 || j== 10-i-1 ){
+                    cout << "*";
                 }
                 else{
-                    cout << "  ";              
+                    cout << " ";              
                 }
                 
             }
-             cout << endl; 
-          }
-         
-           
-         
+             cout << endl;
+          } 
 }

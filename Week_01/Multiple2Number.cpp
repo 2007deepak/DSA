641b139990629b418446ed4381bc854f6fed8b18
Deepak Kumar <dk4503835@gmail.com>
2025-07-23 07:11:01 +0530

    week_01 folder added

---
+++ Week_01/Multiple2Number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a, b, multiply;
+    cout<< "Enter the two numbers:";
+    cin >> a >> b;
+
+    multiply = a* b;
+    cout<< "The multiplication of " << a << "and" << b << " is:" << multiply << endl;
+
+    return 0;
+}

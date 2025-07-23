d8314aa00e57b6bbff6dd4bdd57c170b35116c95
Deepak Kumar <dk4503835@gmail.com>
2025-07-23 07:54:34 +0530

    Print N to 1 counting

---
+++ Week_01/Counting_N_to_1.cpp
@@ -0,0 +1,16 @@
+// Print counting from N to 1
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<< "Enter a Number: ";
+    cin>>n;
+
+    
+    for(int i =n; i>0; i--){
+        cout << i << endl;
+    }
+}
